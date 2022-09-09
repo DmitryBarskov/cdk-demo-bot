8bbbeab7111cef99741bd3ebedb2378eaa7a433d
Dmitry Barskov <dmitry.barskov@flatstack.com>
2022-09-09 18:33:42 +0200

    Refactor

--- bin/pipeline.ts
+++ bin/pipeline.ts
@@ -3,26 +3,25 @@
 import * as cdk from 'aws-cdk-lib';
 import { PipelineStack } from '../lib/pipeline-stack.js';
 
-const ensureEnv = (varName: string): void => {
+const fetchEnv = (varName: string): string => {
   if (!process.env[varName]) {
     throw new Error(`${varName} environment variable is not set`);
   }
-};
 
-ensureEnv('DIL_CDK_SCOPE');
-ensureEnv('CDK_DEFAULT_ACCOUNT');
-ensureEnv('CDK_DEFAULT_REGION');
+  return process.env[varName]!;
+}
 
 const app = new cdk.App();
+
 new PipelineStack(app, 'CdkDemoBotStack', {
-  stackName: `${process.env['DIL_CDK_SCOPE']}-cdk-demo-bot-pipeline`,
-  scope: process.env['DIL_CDK_SCOPE']!,
-  stagingAccout: '676641176260',
-  stagingRegion: 'us-west-2',
-  productionAccount: '676641176260',
-  productionRegion: 'us-west-2',
+  stackName: `${fetchEnv('DIL_CDK_SCOPE')}-cdk-demo-bot-pipeline`,
+  scope: fetchEnv('DIL_CDK_SCOPE'),
+  stagingAccout: fetchEnv('ACCOUNT_ID'),
+  stagingRegion: fetchEnv('REGION'),
+  productionAccount: fetchEnv('ACCOUNT_ID'),
+  productionRegion: fetchEnv('REGION'),
   env: {
-    account: process.env['CDK_DEFAULT_ACCOUNT']!,
-    region: process.env['CDK_DEFAULT_REGION']!
+    account: fetchEnv('CDK_DEFAULT_ACCOUNT'),
+    region: fetchEnv('CDK_DEFAULT_REGION')
   }
 });
